79e1ff29bd3573bd09f8cfc6fc081398d29b5820
Tomás Felipe <tomasfelipe911@gmail.com>
2020-09-15 15:21:51 -0300

    resolucion de conflicto

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,12 +3,7 @@ package proyecto;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
-<<<<<<< HEAD
-		System.out.println("HELLO WORLD!!!!!!");
-=======
-		System.out.println("hola mundo!");
->>>>>>> 1fa754648e2d5845c541e7bfa94cf38cee376d66
+		System.out.println("HELLO WORLD!!!!!"); 
 
 	}
 
